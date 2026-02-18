187fa5c3b3eba42d5c8767883ebb730297ce2d33
kancholover <hyunbin040902@gmail.com>
2026-02-18 06:59:51 +0000

    docker 26/02/18

--- my_fastapi_practice/api/routers/task.py
+++ my_fastapi_practice/api/routers/task.py
@@ -15,16 +15,15 @@ async def list_tasks():
 async def list_tasks():
     return [task_schema.Task(id = 1, title = "첫 번째 ToDo 작업")]
 
-@router.post("/tasks")
-async def create_task():
-    pass
+@router.post("/tasks", response_model = task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate): #요청 본문을 create.task 함수의 인자로 지정
+    return task_schema.TaskCreateResponse(id = 1, **task_body.dict())
 
-
-@router.put("/tasks/{task_id}")
-async def update_task():
-    pass
+@router.put("/tasks/{task_id}", response_model = task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id = task_id, **task_body.dict())
 
 
 @router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass
+async def delete_task(task_id: int):
+    return
--- my_fastapi_practice/api/schemas/task.py
+++ my_fastapi_practice/api/schemas/task.py
@@ -1,8 +1,22 @@
 from pydantic import BaseModel, Field
 
-class Task(BaseModel):
-    id: int
+
+class TaskBase(BaseModel): 
     title: str | None = Field(None, example = "세탁소에 맡긴 것을 찾으러 가기")
+
+class TaskCreate(BaseModel):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description = "완료 플래그")
 
-    
+    class Config:
+        orm_mode = True
+
